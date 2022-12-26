9f1bbdcf10c2d58183bd529443eba4c3296f76db
opekope2 <opekope2@gmail.com>
2022-12-26 22:48:06 +0100

    Add initial mixin

---
+++ src/main/java/opekope2/optigui/mixin/RenderSystemMixin.java
@@ -0,0 +1,16 @@
+package opekope2.optigui.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.util.Identifier;
+import opekope2.optigui.internal.TextureReplacer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(value = RenderSystem.class, /* Inject before Animatica for full compatibility */ priority = 900)
+public final class RenderSystemMixin {
+    @ModifyVariable(method = "_setShaderTexture(ILnet/minecraft/util/Identifier;)V", at = @At("HEAD"), index = 1, argsOnly = true)
+    private static Identifier setShaderTextureMixin(Identifier id) {
+        return TextureReplacer.getReplacement(id);
+    }
+}
---
+++ src/main/kotlin/opekope2/optigui/internal/TextureReplacer.kt
@@ -0,0 +1,10 @@
+package opekope2.optigui.internal
+
+import net.minecraft.util.Identifier
+
+object TextureReplacer {
+    @JvmStatic
+    fun getReplacement(texture: Identifier): Identifier {
+        return texture // TODO
+    }
+}
