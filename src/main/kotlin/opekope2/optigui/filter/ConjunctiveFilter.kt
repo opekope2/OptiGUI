be54d56f7a27ee246cf28bcf18f2be82441b888b
opekope2 <opekope2@gmail.com>
2022-12-27 00:13:10 +0100

    Change conjunctive and disjunctive filters' behavior to return the first replacement if the result is a match
    TODO tests

--- src/main/kotlin/opekope2/optigui/filter/ConjunctiveFilter.kt
+++ src/main/kotlin/opekope2/optigui/filter/ConjunctiveFilter.kt
@@ -2,16 +2,25 @@ package opekope2.optigui.filter
 
 /**
  * A filter which applies the logical AND operation between the given filters and returns the result.
- * Only skips if all sub-filters skip.
+ * Only skips if all sub-filters skip, and only yields match if no sub-filters yield mismatch.
  *
- * This filter doesn't yield a replacement.
+ * This filter yields the first non-skipping mismatch result of all sub-filters if any,
+ * or the first non-skipping match result of all sub-filters if any,
+ * or a new skipping filter result (when all sub-filters skip)
  *
  * @param T The type the filter accepts
  * @param filters The sub-filters to evaluate
  */
 class ConjunctiveFilter<T>(private val filters: Iterable<Filter<T>>) : Filter<T>() {
     override fun test(value: T): FilterResult<out T> = filters.map { it.test(value) }.let { results ->
-        if (results.all { it.skip }) FilterResult(skip = true)
-        else FilterResult(results.none { !it.skip && !it.match })
+        var ret: FilterResult<out T>? = null // First match
+
+        for (res in results) {
+            if (res.skip) continue
+            else if (!res.match) return@let res
+            else if (ret == null) ret = res
+        }
+
+        return@let ret ?: /* all skipped */ FilterResult(skip = true)
     }
 }
--- src/main/kotlin/opekope2/optigui/filter/DisjunctiveFilter.kt
+++ src/main/kotlin/opekope2/optigui/filter/DisjunctiveFilter.kt
@@ -2,16 +2,25 @@ package opekope2.optigui.filter
 
 /**
  * A filter which applies the logical OR operation between the given filters and returns the result.
- * Only skips if all sub-filters skip.
+ * Only skips if all sub-filters skip, and only yields mismatch if no sub-filters yield match.
  *
- * This filter doesn't yield a replacement.
+ * This filter yields the first non-skipping match result of all sub-filters if any,
+ * or the first non-skipping mismatch result of all sub-filters if any,
+ * or a new skipping filter result (when all sub-filters skip)
  *
  * @param T The type the filter accepts
  * @param filters The sub-filters to evaluate
  */
 class DisjunctiveFilter<T>(private val filters: Iterable<Filter<T>>) : Filter<T>() {
     override fun test(value: T): FilterResult<out T> = filters.map { it.test(value) }.let { results ->
-        if (results.all { it.skip }) FilterResult(skip = true)
-        else FilterResult(results.any { !it.skip && it.match })
+        var ret: FilterResult<out T>? = null // First mismatch
+
+        for (res in results) {
+            if (res.skip) continue
+            else if (res.match) return@let res
+            else if (ret == null) ret = res
+        }
+
+        return@let ret ?: /* all skipped */ FilterResult(skip = true)
     }
 }
