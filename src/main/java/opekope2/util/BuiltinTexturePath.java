35a242223d4b2b9b13b6e8e63d50ba8c862a9e00
opekope2 <opekope2@gmail.com>
2022-12-29 20:55:37 +0100

    Add texture path constants of builtin GUIs

---
+++ src/main/java/opekope2/util/BuiltinTexturePath.java
@@ -0,0 +1,26 @@
+package opekope2.util;
+
+import net.minecraft.util.Identifier;
+
+public interface BuiltinTexturePath {
+    Identifier ANVIL = new Identifier("minecraft", "textures/gui/container/anvil.png");
+    Identifier BEACON = new Identifier("minecraft", "textures/gui/container/beacon.png");
+    Identifier BLAST_FURNACE = new Identifier("minecraft", "textures/gui/container/blast_furnace.png");
+    Identifier BREWING_STAND = new Identifier("minecraft", "textures/gui/container/brewing_stand.png");
+    Identifier CARTOGRAPHY_TABLE = new Identifier("minecraft", "textures/gui/container/cartography_table.png");
+    Identifier CHEST = new Identifier("minecraft", "textures/gui/container/generic_54.png");
+    Identifier CRAFTING_TABLE = new Identifier("minecraft", "textures/gui/container/crafting_table.png");
+    Identifier DISPENSER = new Identifier("minecraft", "textures/gui/container/dispenser.png");
+    Identifier ENCHANTING_TABLE = new Identifier("minecraft", "textures/gui/container/enchanting_table.png");
+    Identifier FURNACE = new Identifier("minecraft", "textures/gui/container/furnace.png");
+    Identifier GRINDSTONE = new Identifier("minecraft", "textures/gui/container/grindstone.png");
+    Identifier HOPPER = new Identifier("minecraft", "textures/gui/container/hopper.png");
+    Identifier LOOM = new Identifier("minecraft", "textures/gui/container/loom.png");
+    Identifier SHULKER_BOX = new Identifier("minecraft", "textures/gui/container/shulker_box.png");
+    Identifier SMITHING_TABLE = new Identifier("minecraft", "textures/gui/container/smithing.png");
+    Identifier SMOKER = new Identifier("minecraft", "textures/gui/container/smoker.png");
+    Identifier STONECUTTER = new Identifier("minecraft", "textures/gui/container/stonecutter.png");
+
+    Identifier HORSE = new Identifier("minecraft", "textures/gui/container/horse.png");
+    Identifier VILLAGER = new Identifier("minecraft", "textures/gui/container/villager2.png");
+}
