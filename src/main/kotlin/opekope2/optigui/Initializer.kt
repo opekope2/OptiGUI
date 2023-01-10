51cc60f2cee83a84bebc87a31f5171d9d08a0fee
opekope2 <opekope2@gmail.com>
2023-01-10 01:31:25 +0100

    Prepare adding OptiFine parity features

--- src/main/kotlin/opekope2/optigui/Initializer.kt
+++ src/main/kotlin/opekope2/optigui/Initializer.kt
@@ -6,23 +6,24 @@ import net.fabricmc.fabric.api.event.player.UseBlockCallback
 import net.fabricmc.fabric.api.event.player.UseEntityCallback
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper
 import net.minecraft.MinecraftVersion
+import net.minecraft.block.entity.*
 import net.minecraft.resource.ResourceType
 import opekope2.optigui.exception.UnsupportedMinecraftVersionException
 import opekope2.optigui.internal.InteractionHandler
 import opekope2.optigui.internal.ResourceLoader
-import opekope2.optigui.provider.IRegistryLookupProvider
-import opekope2.optigui.provider.IResourceManagerProvider
-import opekope2.optigui.provider.registerProvider
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 internal val logger: Logger = LoggerFactory.getLogger("OptiGUI")
-const val version = "@mod_version@"
+const val modVersion = "@mod_version@"
 private val gameVersion = MinecraftVersion.CURRENT.name
 
 @Suppress("unused")
 fun initialize() {
-    loadProviders()
+    when (gameVersion) {
+        "1.19.3" -> opekope2.optigui.internal.mc_1_19_3.initialize()
+        else -> throw UnsupportedMinecraftVersionException(modVersion, gameVersion)
+    }
 
     ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(ResourceLoader)
     UseBlockCallback.EVENT.register(InteractionHandler)
@@ -30,17 +31,5 @@ fun initialize() {
     ClientTickEvents.END_WORLD_TICK.register(InteractionHandler)
     ClientPlayConnectionEvents.DISCONNECT.register(InteractionHandler)
 
-    logger.info("OptiGUI $version initialized.")
+    logger.info("OptiGUI $modVersion initialized.")
 }
-
-private fun loadProviders() {
-    when (gameVersion) {
-        "1.19.3" -> {
-            registerProvider<IResourceManagerProvider>(opekope2.optigui.internal.mc_1_19_3.ResourceManagerProvider())
-            registerProvider<IRegistryLookupProvider>(opekope2.optigui.internal.mc_1_19_3.RegistryLookupProvider())
-        }
-
-        else -> throw UnsupportedMinecraftVersionException(version, gameVersion)
-    }
-}
-
