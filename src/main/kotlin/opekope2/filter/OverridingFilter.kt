560ac12fc73351b02aa3c3fad7489f51c17b21fb
opekope2 <opekope2@gmail.com>
2023-01-10 04:55:29 +0100

    Update filter documentation

--- src/main/kotlin/opekope2/filter/OverridingFilter.kt
+++ src/main/kotlin/opekope2/filter/OverridingFilter.kt
@@ -6,10 +6,10 @@ package opekope2.filter
  * Useful to override the sub-filter's result,
  * or when the sub-filter doesn't yield a result (like [ConjunctionFilter] or [DisjunctionFilter])
  *
- * @param filter The filter to evaluate
- * @param result The result to set after evaluating [filter]
  * @param T The type the filter accepts
  * @param TResult the type the filter returns
+ * @param filter The filter to evaluate
+ * @param result The result to set after evaluating [filter]
  */
 class OverridingFilter<T, TResult>(private val filter: Filter<T, out Any>, private val result: TResult) :
     Filter<T, TResult>() {
