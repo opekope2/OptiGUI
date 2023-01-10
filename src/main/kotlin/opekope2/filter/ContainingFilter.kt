ebb524caed221ec3e5ee732523b6bb61345fb30e
opekope2 <opekope2@gmail.com>
2023-01-10 04:59:25 +0100

    Add filter for Collection.contains
    This should be faster than using a ConjunctionFilter with many EqualityFilters

---
+++ src/main/kotlin/opekope2/filter/ContainingFilter.kt
@@ -0,0 +1,12 @@
+package opekope2.filter
+
+/**
+ * A filter, which only yields a matching result if the given [collection] contains the input.
+ * This filter never skips.
+ *
+ * @param T The type the filter accepts
+ * @param collection The collection to check for the input
+ */
+class ContainingFilter<T, Unit>(private val collection: Collection<T>) : Filter<T, Unit>() {
+    override fun test(value: T): FilterResult<out Unit> = FilterResult(skip = false, match = value in collection)
+}
