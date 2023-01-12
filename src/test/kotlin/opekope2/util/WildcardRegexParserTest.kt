e832ef699ba493d61b5041bfccb012ac3f5112c8
opekope2 <opekope2@gmail.com>
2023-01-12 02:47:01 +0100

    Fix OptiFine regex parsing test
    Added in 50c460eb5634b9c9e550cec589076ce9aeb813b1

--- src/test/kotlin/opekope2/util/WildcardRegexParserTest.kt
+++ src/test/kotlin/opekope2/util/WildcardRegexParserTest.kt
@@ -9,56 +9,55 @@ class WildcardRegexParserTest {
     fun parseExactStringTest() {
         val filter = parseWildcardOrRegex("Exact String")
 
-        assertFalse(filter.test("Exact String").skip)
-        assertTrue(filter.test("Exact String").match)
-        assertFalse(filter.test("Exact string").match)
-        assertFalse(filter.test("exact string").match)
-        assertFalse(filter.test("EXACT STRING").match)
-        assertFalse(filter.test("Something Different").match)
+        assertTrue(filter.matches("Exact String"))
+        assertFalse(filter.matches("Exact string"))
+        assertFalse(filter.matches("exact string"))
+        assertFalse(filter.matches("EXACT STRING"))
+        assertFalse(filter.matches("Something Different"))
     }
 
     @Test
     fun parsePatternTest() {
         val filter = parseWildcardOrRegex("pattern:?Letter to *")
 
-        assertTrue(filter.test("Letter to Herobrine").match)
-        assertTrue(filter.test("Letter to a creeper").match)
-        assertTrue(filter.test("My Letter to John").match)
-        assertFalse(filter.test("letter to Herobrine").match)
-        assertFalse(filter.test("Letter from Herobrine").match)
+        assertTrue(filter.matches("Letter to Herobrine"))
+        assertTrue(filter.matches("Letter to a creeper"))
+        assertTrue(filter.matches("My Letter to John"))
+        assertFalse(filter.matches("letter to Herobrine"))
+        assertFalse(filter.matches("Letter from Herobrine"))
     }
 
     @Test
     fun parseIPatternTest() {
         val filter = parseWildcardOrRegex("ipattern:Letter to *")
 
-        assertTrue(filter.test("Letter to Herobrine").match)
-        assertTrue(filter.test("Letter to a creeper").match)
-        assertTrue(filter.test("letter to Herobrine").match)
-        assertTrue(filter.test("letter to STEVE").match)
-        assertFalse(filter.test("A letter to CJ").match)
-        assertFalse(filter.test("Letter from Herobrine").match)
+        assertTrue(filter.matches("Letter to Herobrine"))
+        assertTrue(filter.matches("Letter to a creeper"))
+        assertTrue(filter.matches("letter to Herobrine"))
+        assertTrue(filter.matches("letter to STEVE"))
+        assertFalse(filter.matches("A letter to CJ"))
+        assertFalse(filter.matches("Letter from Herobrine"))
     }
 
     @Test
     fun parseRegexTest() {
         val filter = parseWildcardOrRegex("regex:Letter (to|from) .*")
 
-        assertTrue(filter.test("Letter to Herobrine").match)
-        assertTrue(filter.test("Letter from Herobrine").match)
-        assertFalse(filter.test("letter to Herobrine").match)
-        assertFalse(filter.test("A Letter to Herobrine").match)
+        assertTrue(filter.matches("Letter to Herobrine"))
+        assertTrue(filter.matches("Letter from Herobrine"))
+        assertFalse(filter.matches("letter to Herobrine"))
+        assertFalse(filter.matches("A Letter to Herobrine"))
     }
 
     @Test
     fun parseIRegexTest() {
         val filter = parseWildcardOrRegex("iregex:Letter (to|from) .*")
 
-        assertTrue(filter.test("Letter to Herobrine").match)
-        assertTrue(filter.test("Letter from Herobrine").match)
-        assertTrue(filter.test("letter to Herobrine").match)
-        assertTrue(filter.test("LETTER TO HEROBRINE").match)
-        assertFalse(filter.test("A Letter to Herobrine").match)
-        assertFalse(filter.test("LETTER TOFROM HEROBRINE").match)
+        assertTrue(filter.matches("Letter to Herobrine"))
+        assertTrue(filter.matches("Letter from Herobrine"))
+        assertTrue(filter.matches("letter to Herobrine"))
+        assertTrue(filter.matches("LETTER TO HEROBRINE"))
+        assertFalse(filter.matches("A Letter to Herobrine"))
+        assertFalse(filter.matches("LETTER TOFROM HEROBRINE"))
     }
 }
