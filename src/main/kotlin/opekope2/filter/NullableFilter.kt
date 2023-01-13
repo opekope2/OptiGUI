7bdc8255de3a4fa251481e32342f3df92806722f
opekope2 <opekope2@gmail.com>
2023-01-13 02:21:11 +0100

    Make member private in NullableFilter

--- src/main/kotlin/opekope2/filter/NullableFilter.kt
+++ src/main/kotlin/opekope2/filter/NullableFilter.kt
@@ -11,9 +11,9 @@ package opekope2.filter
  * @param filter The filter to evaluate
  */
 class NullableFilter<T, TResult>(
-    val skipOnNull: Boolean,
-    val failOnNull: Boolean,
-    val filter: Filter<T, TResult>
+    private val skipOnNull: Boolean,
+    private val failOnNull: Boolean,
+    private val filter: Filter<T, TResult>
 ) : Filter<T?, TResult>() {
     override fun evaluate(value: T?): FilterResult<out TResult> =
         if (value == null) FilterResult(skip = skipOnNull, match = !failOnNull)
