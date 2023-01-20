a32826e56355ea84f5a42c2a8dd994afb7b1fcb7
opekope2 <opekope2@gmail.com>
2023-01-20 03:58:49 +0100

    Update dispenser code to the new filtering system

--- src/main/kotlin/opekope2/optigui/internal/mc_all/Dispenser.kt
+++ src/main/kotlin/opekope2/optigui/internal/mc_all/Dispenser.kt
@@ -28,14 +28,15 @@ fun createDispenserFilter(resource: Resource): FilterInfo? {
     val filters = createGeneralFilters(resource, container, texture)
 
     filters.addForProperty(resource, "variants", { it.splitIgnoreEmpty(*delimiters) }) { variants ->
-        TransformationFilter(
-            { (it.data as? DispenserProperties)?.variant },
-            ContainingFilter(variants) // variants can't contain null
-        )
+        val variantFilter = ContainingFilter(variants)
+
+        Filter {
+            variantFilter.evaluate((it.data as? DispenserProperties)?.variant ?: return@Filter FilterResult.Mismatch())
+        }
     }
 
     return FilterInfo(
-        OverridingFilter(ConjunctionFilter(filters), replacement),
+        PostProcessorFilter(ConjunctionFilter(filters), replacement),
         setOf(texture)
     )
 }
