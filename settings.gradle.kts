3174215b979c6c67e0c72fdcdfe7600e8d0f8438
opekope2 <opekope2@gmail.com>
2023-03-11 01:31:57 +0100

    Mark Quilt-supported
    Bump version
    Edit publish script
    Change optifabric to conflicts
    Edit settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
     }
     plugins {
         id("fabric-loom") version settings.extra["loom_version"] as String apply false
-        kotlin("jvm") version System.getProperty("kotlin_version")
+        kotlin("jvm") version System.getProperty("kotlin_version") apply false
         id("net.kyori.blossom") version "1.3.1" apply false
         id("org.jetbrains.dokka") version "1.8.10" apply false
     }
