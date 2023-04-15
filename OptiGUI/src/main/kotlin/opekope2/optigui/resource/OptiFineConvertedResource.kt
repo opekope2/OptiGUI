0539f6839d43560f02612997576c80f16123bb56
opekope2 <opekope2@gmail.com>
2023-04-15 03:13:41 +0200

    Merge resolvePath into OptiFine converter

--- OptiGUI/src/main/kotlin/opekope2/optigui/resource/OptiFineConvertedResource.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/resource/OptiFineConvertedResource.kt
@@ -6,13 +6,13 @@ import opekope2.optigui.internal.optifinecompat.delimiters
 import opekope2.optigui.service.ResourceAccessService
 import opekope2.optigui.service.getService
 import opekope2.util.NumberOrRange
-import opekope2.util.resolvePath
 import opekope2.util.splitIgnoreEmpty
 import opekope2.util.toBoolean
 import org.ini4j.Ini
 import org.ini4j.Options
 import org.ini4j.Profile.Section
 import java.io.File
+import java.util.*
 
 /**
  * Represents an OptiFine .properties file converted to the OptiGUI-compatible INI format.
@@ -171,6 +171,52 @@ private fun resolveReplacementTexture(texture: String, resourcePath: Identifier)
     }
 
     return texturePath.toString()
+}
+
+private fun resolvePath(resourcePath: String, path: String): Identifier? {
+    val pathStack: Deque<String> = ArrayDeque()
+    var tokenizer = StringTokenizer(resourcePath, "/")
+    while (tokenizer.hasMoreTokens()) {
+        pathStack.push(tokenizer.nextToken())
+    }
+
+    // Because there was a resource pack with two dangling tab characters after the resource name
+    tokenizer = StringTokenizer(path.trim(), ":/", true)
+    var namespace = Identifier.DEFAULT_NAMESPACE
+    var nToken = -1
+
+    while (tokenizer.hasMoreTokens()) {
+        val token = tokenizer.nextToken()
+        nToken++
+
+        if (nToken == 0 && "~" == token) {
+            pathStack.clear()
+            pathStack.push("optifine")
+            continue
+        } else if (":" == token) {
+            if (nToken == 1) {
+                namespace = pathStack.pop()
+                pathStack.clear()
+            }
+        } else if (".." == token) {
+            if (pathStack.isEmpty()) return null
+
+            pathStack.pop()
+        } else if ("/" != token && "." != token) {
+            pathStack.push(token)
+        }
+    }
+
+    val pathBuilder = StringBuilder()
+    var first = true
+
+    while (!pathStack.isEmpty()) {
+        if (first) first = false
+        else pathBuilder.append("/")
+        pathBuilder.append(pathStack.removeLast())
+    }
+
+    return Identifier(namespace, pathBuilder.toString())
 }
 
 private fun Options.copyTo(target: Section, vararg keyMap: Pair<String, String>) {
