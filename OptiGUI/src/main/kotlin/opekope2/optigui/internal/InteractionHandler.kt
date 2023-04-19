aa25bd0ef89aa0af297bd1ddc1d5d6209726df4a
opekope2 <opekope2@gmail.com>
2023-04-19 22:33:58 +0200

    Add support for non-block entity block GUIs

--- OptiGUI/src/main/kotlin/opekope2/optigui/internal/InteractionHandler.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/internal/InteractionHandler.kt
@@ -8,10 +8,14 @@ import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.hit.EntityHitResult
+import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import opekope2.optigui.interaction.InteractionTarget
+import opekope2.optigui.properties.DefaultProperties
 import opekope2.optigui.service.InteractionService
+import opekope2.optigui.service.RegistryLookupService
 import opekope2.optigui.service.getService
+import opekope2.util.TexturePath
 
 internal object InteractionHandler : UseBlockCallback, UseEntityCallback {
     private val interactor = getService<InteractionService>()
@@ -21,12 +25,31 @@ internal object InteractionHandler : UseBlockCallback, UseEntityCallback {
             val blockEntity = world.getBlockEntity(hitResult.blockPos)
             val target =
                 if (blockEntity != null) InteractionTarget.BlockEntity(blockEntity)
-                else InteractionTarget.None
+                else getBlockInteractionTarget(world, hitResult.blockPos)
 
             interactor.interact(player, world, hand, target, hitResult)
         }
 
         return ActionResult.PASS
+    }
+
+    private fun getBlockInteractionTarget(world: World, target: BlockPos): InteractionTarget {
+        val lookup = getService<RegistryLookupService>()
+
+        val container = lookup.lookupBlockId(world.getBlockState(target).block)
+        if (TexturePath.ofContainer(container) == null) {
+            // Unknown/modded container
+            return InteractionTarget.None
+        }
+
+        return InteractionTarget.Preprocessed(
+            DefaultProperties(
+                container = container,
+                name = null,
+                biome = lookup.lookupBiomeId(world, target),
+                height = target.y
+            )
+        )
     }
 
     override fun interact(
