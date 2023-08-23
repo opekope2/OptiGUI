1efc7644c5d51df1317959a7b8038f02301f4f3e
opekope2 <opekope2@gmail.com>
2023-08-23 22:47:46 +0200

    Fix OptiGUI deps
    Include the other projects code

--- OptiGUI/build.gradle.kts
+++ OptiGUI/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     kotlin("jvm")
 }
 
+evaluationDependsOn(":Api")
+evaluationDependsOn(":Filters")
+evaluationDependsOn(":Properties")
+
 base { archivesName.set(project.extra["archives_base_name"] as String) }
 
 version = project.extra["mod_version"] as String
@@ -12,21 +16,21 @@ repositories {}
 
 dependencies {
     minecraft("com.mojang", "minecraft", project.extra["minecraft_version"] as String)
-    mappings("net.fabricmc", "yarn", project.extra["yarn_mappings"] as String, null, "v2")
+    mappings("net.fabricmc", "yarn", project.extra["yarn_mappings"] as String, classifier = "v2")
     modImplementation("net.fabricmc", "fabric-loader", project.extra["loader_version"] as String)
     modImplementation(
         "net.fabricmc", "fabric-language-kotlin", project.extra["fabric_language_kotlin_version"] as String
     )
 
-    (project.extra["fabric_version"] as String).also { fabricVersion ->
-        modImplementation(fabricApi.module("fabric-lifecycle-events-v1", fabricVersion))
-        modImplementation(fabricApi.module("fabric-networking-api-v1", fabricVersion))
-        modImplementation(fabricApi.module("fabric-events-interaction-v0", fabricVersion))
-    }
+    modImplementation(fabricApi.module("fabric-networking-api-v1", project.extra["fabric_version"] as String))
+    modImplementation(fabricApi.module("fabric-events-interaction-v0", project.extra["fabric_version"] as String))
 
-    if (!project.hasProperty("noModLocalRuntime")) {
-        modLocalRuntime(project(":OptiGlue:1.19.3", configuration = "namedElements"))
-    }
+    modImplementation(files(rootDir.resolve("lib/lilac-api-1.0.0-alpha.1-dev.jar")))
+    modLocalRuntime(files(rootDir.resolve("lib/lilac-1.0.0-alpha.1-dev.jar")))
+
+    implementation(project(":Api", configuration = "namedElements"))
+    implementation(project(":Filters"))
+    implementation(project(":Properties", configuration = "namedElements"))
 
     include(implementation("org.apache.commons", "commons-text", "1.10.0"))
     include(implementation("org.ini4j", "ini4j", "0.5.4"))
@@ -57,6 +61,18 @@ tasks {
     jar {
         from("LICENSE") {
             rename { "${it}_${base.archivesName.get()}" }
+        }
+        from(project(":Api").sourceSets["main"].output) {
+            include("**/*.class")
+            duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        }
+        from(project(":Filters").sourceSets["main"].output) {
+            include("**/*.class")
+            include("**/*.kotlin_module")
+        }
+        from(project(":Properties").sourceSets["main"].output) {
+            include("**/*.class")
+            include("**/*.kotlin_module")
         }
     }
 
