661e31d392e56baa0066d92f17771d1737c762e3
opekope2 <opekope2@gmail.com>
2023-08-24 16:03:00 +0200

    Remove deprecated code
    Moved to LiLaC, other subprojects, or simply no longer used

--- OptiGUI/src/main/kotlin/opekope2/optigui/internal/service/ResourceLoaderService.kt
+++
@@ -1,7 +0,0 @@
-package opekope2.optigui.internal.service
-
-import opekope2.optigui.resource.OptiGuiResource
-
-interface ResourceLoaderService {
-    fun loadResources(resources: Set<OptiGuiResource>)
-}
--- OptiGUI/src/main/kotlin/opekope2/util/Util.kt
+++ OptiGUI/src/main/kotlin/opekope2/util/Util.kt
@@ -4,32 +4,7 @@ import net.minecraft.client.gui.screen.ingame.LecternScreen
 import net.minecraft.screen.*
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.MathHelper
-import opekope2.filter.FilterResult
 import java.io.StringWriter
-
-/**
- * Converts the given string to a boolean:
- * - `true`, if the string is "true" (case-insensitive)
- * - `false`, if the string is "false" (case-insensitive)
- * - `null` otherwise
- */
-internal fun String?.toBoolean(): Boolean? {
-    return (this ?: return null).lowercase().toBooleanStrictOrNull()
-}
-
-/**
- * If the current [FilterResult] is [FilterResult.Match], change its result to the given one.
- * Otherwise, return the original.
- *
- * @param TOld The type of the old result
- * @param TNew The type of the new result
- * @param result The new result
- */
-fun <TOld, TNew> FilterResult<TOld>.withResult(result: TNew): FilterResult<TNew> = when (this) {
-    is FilterResult.Skip -> FilterResult.Skip()
-    is FilterResult.Mismatch -> FilterResult.Mismatch()
-    is FilterResult.Match -> FilterResult.Match(result)
-}
 
 /**
  * Trim parentheses from the start and end of a string.
