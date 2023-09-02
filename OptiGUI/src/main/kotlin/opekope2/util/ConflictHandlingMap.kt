46f31139cf844ac155851d77b4d9b9ffbcf3040c
opekope2 <opekope2@gmail.com>
2023-09-02 18:19:58 +0200

    Implement Map interface into ConflictHandlingMap

--- OptiGUI/src/main/kotlin/opekope2/util/ConflictHandlingMap.kt
+++ OptiGUI/src/main/kotlin/opekope2/util/ConflictHandlingMap.kt
@@ -2,8 +2,26 @@ package opekope2.util
 
 import opekope2.lilac.util.Tree
 
-internal class ConflictHandlingMap<TKey, TValue> {
+internal class ConflictHandlingMap<TKey, TValue> : Map<TKey, TValue> {
     private val map = mutableMapOf<TKey, MutableList<Pair<String, TValue>>>()
+
+    override val size: Int
+        get() = map.size
+
+    override val keys: Set<TKey>
+        get() = map.keys
+
+    override val values: Collection<TValue>
+        get() {
+            ensureNoConflicts()
+            return map.map { (_, values) -> values.first().second }
+        }
+
+    override val entries: Set<Map.Entry<TKey, TValue>>
+        get() {
+            ensureNoConflicts()
+            return map.mapTo(mutableSetOf()) { (key, values) -> Entry(key, values.first().second) }
+        }
 
     var conflicts = false
         private set
@@ -14,22 +32,24 @@ internal class ConflictHandlingMap<TKey, TValue> {
         }
     }
 
+    override fun containsKey(key: TKey): Boolean = key in map
+
+    override fun containsValue(value: TValue): Boolean =
+        map.any { (_, values) -> values.any { (_, v) -> v == value } }
+
+    override fun get(key: TKey): TValue? {
+        ensureNoConflicts()
+        return map[key]?.first()?.second
+    }
+
+    override fun isEmpty(): Boolean = map.isEmpty()
+
     fun put(key: TKey, modId: String, value: TValue) {
         if (key in map) {
             conflicts = true
         }
 
         map.getOrPut(key) { mutableListOf() }.add(modId to value)
-    }
-
-    fun get(key: TKey): TValue? {
-        ensureNoConflicts()
-        return map[key]?.first()?.second
-    }
-
-    fun toMap(): Map<TKey, TValue> {
-        ensureNoConflicts()
-        return map.mapValues { (_, value) -> value.first().second }
     }
 
     fun createConflictTree(rootText: String): Tree.Node {
@@ -47,4 +67,6 @@ internal class ConflictHandlingMap<TKey, TValue> {
 
         return root
     }
+
+    private class Entry<TKey, TValue>(override val key: TKey, override val value: TValue) : Map.Entry<TKey, TValue>
 }
