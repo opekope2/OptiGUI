d85b55a301e5e6f887d999b41978534310eb1dd7
opekope2 <opekope2@gmail.com>
2023-09-02 19:05:19 +0200

    Reimplement chest boat variant selector

---
+++ OptiGUI/src/main/kotlin/opekope2/optigui/internal/selector/ChestBoatSelectors.kt
@@ -0,0 +1,35 @@
+package opekope2.optigui.internal.selector
+
+import net.minecraft.entity.vehicle.BoatEntity
+import opekope2.optigui.annotation.Selector
+import opekope2.optigui.api.interaction.Interaction
+import opekope2.optigui.api.selector.ISelector
+import opekope2.optigui.filter.ContainingFilter
+import opekope2.optigui.filter.Filter
+import opekope2.optigui.filter.FilterResult
+import opekope2.optigui.filter.PreProcessorFilter
+import opekope2.util.assertNotEmpty
+import opekope2.util.joinNotFound
+import opekope2.util.map
+import opekope2.optigui.properties.IChestBoatProperties
+import opekope2.util.delimiters
+import opekope2.util.splitIgnoreEmpty
+
+
+@Selector("chest_boat.variants")
+class ChestBoatVariantSelector : ISelector {
+    override fun createFilter(selector: String): Filter<Interaction, *>? =
+        selector.splitIgnoreEmpty(*delimiters)
+            ?.assertNotEmpty()
+            ?.map({ variant -> variant.takeIf { BoatEntity.Type.CODEC.byId(variant) != null } }) {
+                throw RuntimeException("Invalid chest boat variants: ${joinNotFound(it)}")
+            }
+            ?.assertNotEmpty()
+            ?.let { variants ->
+                PreProcessorFilter.nullGuarded(
+                    { (it.data as? IChestBoatProperties)?.variant },
+                    FilterResult.Mismatch(),
+                    ContainingFilter(variants)
+                )
+            }
+}
