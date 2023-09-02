77893e9fd598b05bf203d536dafb6f9693555e7a
opekope2 <opekope2@gmail.com>
2023-09-02 19:10:21 +0200

    Reimplement villager selectors

---
+++ OptiGUI/src/main/kotlin/opekope2/optigui/internal/selector/VillagerSelectors.kt
@@ -0,0 +1,74 @@
+package opekope2.optigui.internal.selector
+
+import net.minecraft.util.Identifier
+import opekope2.optigui.annotation.Selector
+import opekope2.optigui.api.interaction.Interaction
+import opekope2.optigui.api.selector.ISelector
+import opekope2.optigui.filter.*
+import opekope2.util.assertNotEmpty
+import opekope2.util.joinNotFound
+import opekope2.util.map
+import opekope2.optigui.properties.IVillagerProperties
+import opekope2.util.NumberOrRange
+import opekope2.util.delimiters
+import opekope2.util.splitIgnoreEmpty
+
+
+@Selector("villager.profession")
+class VillagerProfessionSelector : ISelector {
+    override fun createFilter(selector: String): Filter<Interaction, *>? =
+        selector.splitIgnoreEmpty(*delimiters)
+            ?.assertNotEmpty()
+            ?.map({ profession ->
+                if ('@' in profession) {
+                    if (profession.count { it == '@' } > 2) return@map null
+                    val (profId, profLevel) = profession.split('@')
+                    val id = Identifier.tryParse(profId) ?: return@map null
+                    val level = NumberOrRange.tryParse(profLevel) ?: return@map null
+                    id to level
+                } else {
+                    (Identifier.tryParse(profession) ?: return@map null) to null
+                }
+            }) { throw RuntimeException("Invalid professions: ${joinNotFound(it)}") }
+            ?.assertNotEmpty()
+            ?.let { professions ->
+                DisjunctionFilter(
+                    professions.map { (profession, level) ->
+                        val profFilter = PreProcessorFilter.nullGuarded<Interaction, Identifier, Unit>(
+                            { (it.data as? IVillagerProperties)?.profession },
+                            FilterResult.Mismatch(),
+                            EqualityFilter(profession)
+                        )
+                        val levelFilter = level?.toFilter()
+
+                        if (levelFilter == null) profFilter
+                        else ConjunctionFilter(
+                            profFilter,
+                            PreProcessorFilter.nullGuarded(
+                                { (it.data as? IVillagerProperties)?.level },
+                                FilterResult.Mismatch(),
+                                levelFilter
+                            )
+                        )
+                    }
+                )
+            }
+}
+
+@Selector("villager.type")
+class VillagerTypeSelector : ISelector {
+    override fun createFilter(selector: String): Filter<Interaction, *>? =
+        selector.splitIgnoreEmpty(*delimiters)
+            ?.assertNotEmpty()
+            ?.map(Identifier::tryParse) {
+                throw RuntimeException("Invalid villager types: ${joinNotFound(it)}")
+            }
+            ?.assertNotEmpty()
+            ?.let { types ->
+                PreProcessorFilter.nullGuarded(
+                    { (it.data as? IVillagerProperties)?.type },
+                    FilterResult.Mismatch(),
+                    ContainingFilter(types)
+                )
+            }
+}
