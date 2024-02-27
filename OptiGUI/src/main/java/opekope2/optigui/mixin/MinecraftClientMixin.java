9a33769bf3c5e92bc25b50c4cc0edf102c047ab6
opekope2 <opekope2@gmail.com>
2024-02-28 00:26:45 +0100

    Merge MinecraftClientMixin2 into MinecraftClientMixin
    There is an even, which makes this separation redundant
    Modernize and make code cleaner by using shadow

--- OptiGUI/src/main/java/opekope2/optigui/mixin/MinecraftClientMixin.java
+++ OptiGUI/src/main/java/opekope2/optigui/mixin/MinecraftClientMixin.java
@@ -2,17 +2,32 @@ package opekope2.optigui.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayerEntity;
 import opekope2.optigui.internal.TextureReplacer;
+import opekope2.optigui.internal.interaction.InteractionHandler;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = MinecraftClient.class)
 abstract class MinecraftClientMixin {
+    @Shadow
+    @Nullable
+    public ClientPlayerEntity player;
+
+    @Shadow
+    @Nullable
+    public Screen currentScreen;
+
     @Inject(method = "setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At("TAIL"))
     private void setScreenMixin(Screen screen, CallbackInfo ci) {
-        MinecraftClient thiz = (MinecraftClient) (Object) this;
-        TextureReplacer.handleScreenChange(thiz.currentScreen);
+        if (player != null && player.getEntityWorld() != null && currentScreen != null) {
+            InteractionHandler.interact(player, player.getEntityWorld(), currentScreen);
+        }
+
+        TextureReplacer.handleScreenChange(currentScreen);
     }
 }
