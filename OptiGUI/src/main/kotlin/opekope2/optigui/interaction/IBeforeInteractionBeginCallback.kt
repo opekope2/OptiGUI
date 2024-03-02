53e1d2cbf0ff44c316e2c6112b36f736ad905288
opekope2 <opekope2@gmail.com>
2024-03-02 21:46:50 +0100

    Add screen parameter to IBeforeInteractionBeginCallback

--- OptiGUI/src/main/kotlin/opekope2/optigui/interaction/IBeforeInteractionBeginCallback.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/interaction/IBeforeInteractionBeginCallback.kt
@@ -2,6 +2,7 @@ package opekope2.optigui.interaction
 
 import net.fabricmc.fabric.api.event.Event
 import net.fabricmc.fabric.api.event.EventFactory
+import net.minecraft.client.gui.screen.Screen
 
 /**
  * Callback fired before an interaction begins.
@@ -9,18 +10,22 @@ import net.fabricmc.fabric.api.event.EventFactory
 fun interface IBeforeInteractionBeginCallback {
     /**
      * Called right before an interaction begins. This is the last place when calling [Interaction.prepare] is valid.
+     *
+     * @param screen The screen, which triggered the interaction and is about to be opened
      */
-    fun onBeforeBegin()
+    fun onBeforeBegin(screen: Screen)
 
-    companion object{
+    companion object {
         /**
          * Event fired before an interaction begins.
          */
         @JvmField
         val EVENT: Event<IBeforeInteractionBeginCallback> =
             EventFactory.createArrayBacked(IBeforeInteractionBeginCallback::class.java) { listeners ->
-                IBeforeInteractionBeginCallback {
-                    listeners.forEach(IBeforeInteractionBeginCallback::onBeforeBegin)
+                IBeforeInteractionBeginCallback { screen ->
+                    listeners.forEach { listener ->
+                        listener.onBeforeBegin(screen)
+                    }
                 }
             }
     }
