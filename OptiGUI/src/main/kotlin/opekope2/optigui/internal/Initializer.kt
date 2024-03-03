cb33b95598be375866ce727fa69e7a84f50d0072
opekope2 <opekope2@gmail.com>
2024-03-03 18:49:46 +0100

    Add OptiGUI INI filter loader

--- OptiGUI/src/main/kotlin/opekope2/optigui/internal/Initializer.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/internal/Initializer.kt
@@ -1,22 +1,12 @@
 package opekope2.optigui.internal
 
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper
-import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.client.gui.screen.ingame.BookEditScreen
 import net.minecraft.client.gui.screen.ingame.BookScreen
 import net.minecraft.client.gui.screen.ingame.HandledScreen
 import net.minecraft.client.gui.screen.ingame.HangingSignEditScreen
-import net.minecraft.resource.ResourceType
+import opekope2.optigui.registry.FilterLoaderRegistry
 import opekope2.optigui.registry.RetexturableScreenRegistry
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import kotlin.jvm.optionals.getOrNull
-
-internal val logger: Logger = LoggerFactory.getLogger("OptiGUI")
-val modVersion =
-    FabricLoader.getInstance().getModContainer("optigui").getOrNull()?.metadata?.version
-        ?: throw RuntimeException("OptiGUI is not loaded with id 'optigui'!")
-
+import opekope2.optigui.resource.OptiGuiFilterLoader
 
 @Suppress("unused")
 fun initialize() {
@@ -25,7 +15,5 @@ fun initialize() {
     RetexturableScreenRegistry.register(BookEditScreen::class.java)
     RetexturableScreenRegistry.register(HangingSignEditScreen::class.java)
 
-    ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(ResourceLoader)
-
-    logger.info("OptiGUI initialized.")
+    FilterLoaderRegistry.register(OptiGuiFilterLoader())
 }
