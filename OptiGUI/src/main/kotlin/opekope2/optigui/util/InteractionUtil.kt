b5700d4f166ba17428bbb18587837aeda1c6ffe5
opekope2 <opekope2@gmail.com>
2024-04-24 13:53:06 +0200

    Disable block entity NBT dump in inspector on Minecraft 1.20.5
    This is the only breaking change in Minecraft 1.20.5, so I'll add a temporary tech debt so support Minecraft 1.20.2-1.20.5 with a single JAR

--- OptiGUI/src/main/kotlin/opekope2/optigui/util/InteractionUtil.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/util/InteractionUtil.kt
@@ -2,6 +2,8 @@
 
 package opekope2.optigui.util
 
+import net.fabricmc.loader.api.FabricLoader
+import net.fabricmc.loader.api.Version
 import net.minecraft.nbt.NbtCompound
 import opekope2.optigui.interaction.Interaction
 import opekope2.optigui.internal.TextureReplacer
@@ -18,6 +20,10 @@ val interactionData: Interaction.Data?
  * current screen pauses the game in single player.
  */
 fun invalidateCachedReplacement() = TextureReplacer.clearReplacementCache()
+
+// FIXME tech debt
+private val minecraft_1_20_5 =
+    FabricLoader.getInstance().getModContainer("minecraft").get().metadata.version >= Version.parse("1.20.5")
 
 /**
  * Creates an OptiGUI INI from the current interaction data.
@@ -39,7 +45,14 @@ fun inspectInteraction(): String? {
         appendLine()
 
         when {
-            data.blockEntity != null -> {
+            // FIXME tech debt
+            data.blockEntity != null && minecraft_1_20_5 -> {
+                appendLine("# Block entity NBT is not available on Minecraft 1.20.5+")
+                appendLine()
+            }
+
+            // FIXME tech debt
+            data.blockEntity != null && !minecraft_1_20_5 -> {
                 appendLine("# NBT (for more detailed inspection purposes, as there is no selector for NBT):")
                 appendLine("# ${data.blockEntity.createNbt()}")
                 appendLine()
