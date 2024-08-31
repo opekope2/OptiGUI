58570f02323cae86ee632de2bd6d9c9618859733
opekope2 <opekope2@gmail.com>
2024-08-31 16:25:12 +0200

    Move collection operators outside collection filter

--- OptiGUI/src/main/kotlin/opekope2/optigui/filter/CollectionFilter.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/filter/CollectionFilter.kt
@@ -1,18 +1,7 @@
 package opekope2.optigui.filter
 
 import com.google.common.collect.ImmutableList
-import java.util.function.BiPredicate
-import java.util.function.Predicate
-
-/**
- * A function, which decides if a filter matches based on the results of a collection of filters.
- *
- * @see CollectionFilter.none
- * @see CollectionFilter.any
- * @see CollectionFilter.some
- * @see CollectionFilter.all
- */
-typealias ICollectionOperator<T> = BiPredicate<Collection<Predicate<T>>, T>
+import opekope2.optigui.util.IPredicateCollectionOperator
 
 /**
  * A collection of multiple sub-filters.
@@ -22,44 +11,12 @@ typealias ICollectionOperator<T> = BiPredicate<Collection<Predicate<T>>, T>
  */
 class CollectionFilter<TFilter : IFilter<TInput>, TInput>(
     private val filters: ImmutableList<TFilter>,
-    private val operator: ICollectionOperator<TInput>
+    private val operator: IPredicateCollectionOperator<TInput>
 ) : IFilter<TInput>, Iterable<TFilter> {
-    constructor(filters: Collection<TFilter>, operator: ICollectionOperator<TInput>) :
+    constructor(filters: Collection<TFilter>, operator: IPredicateCollectionOperator<TInput>) :
             this(ImmutableList.copyOf(filters), operator)
 
     override fun iterator() = filters.iterator()
 
     override fun test(value: TInput) = operator.test(filters, value)
-
-    @Suppress("UNCHECKED_CAST")
-    companion object {
-        private val none = ICollectionOperator<Any?> { predicates, testValue -> predicates.none { it.test(testValue) } }
-        private val any = ICollectionOperator<Any?> { predicates, testValue -> predicates.any { it.test(testValue) } }
-        private val some = ICollectionOperator<Any?> { predicates, testValue -> !predicates.all { it.test(testValue) } }
-        private val all = ICollectionOperator<Any?> { predicates, testValue -> predicates.all { it.test(testValue) } }
-
-        /**
-         * Matches only if no elements match in the collection. Matches empty collections.
-         */
-        @JvmStatic
-        fun <T> none() = none as ICollectionOperator<T>
-
-        /**
-         * Matches if at least 1 element matches in the collection. Doesn't match empty collections.
-         */
-        @JvmStatic
-        fun <T> any() = any as ICollectionOperator<T>
-
-        /**
-         * Matches if 0 or more, but not all elements match in the collection. Doesn't match empty collections.
-         */
-        @JvmStatic
-        fun <T> some() = some as ICollectionOperator<T>
-
-        /**
-         * Matches if every single element matches in the collection. Matches empty collections.
-         */
-        @JvmStatic
-        fun <T> all() = all as ICollectionOperator<T>
-    }
 }
