9f6b7d7b0908b90c729d09fdd052ba35e8054a8e
opekope2 <opekope2@gmail.com>
2024-09-21 00:27:37 +0200

    Change ISingleParameterNbtMatcher.createFilter to use require

--- OptiGUI/src/main/kotlin/opekope2/optigui/resource/matcher/nbt/ISingleParameterNbtMatcher.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/resource/matcher/nbt/ISingleParameterNbtMatcher.kt
@@ -1,10 +1,13 @@
 package opekope2.optigui.resource.matcher.nbt
 
+import opekope2.optigui.filter.INbtFilter
+
 /**
  * An NBT matcher, which takes exactly 1 parameter.
  */
 fun interface ISingleParameterNbtMatcher : INbtMatcher {
-    override fun createFilter(parameters: List<Any?>) =
-        if (parameters.size == 1) createFilter(parameters[0])
-        else throw IllegalArgumentException("Exactly 1 parameter is required, but ${parameters.size} was supplied")
+    override fun createFilter(parameters: List<Any?>): INbtFilter {
+        require(parameters.size == 1) { "Exactly 1 parameter is required, but ${parameters.size} was supplied" }
+        return createFilter(parameters[0])
+    }
 }
