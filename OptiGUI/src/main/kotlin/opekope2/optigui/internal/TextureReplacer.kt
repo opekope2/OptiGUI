40c795beef4454bd27b637d8e73d994ccff8ab51
opekope2 <opekope2@gmail.com>
2024-09-22 21:15:20 +0200

    Replace GameRendererMixin with Fabric events

--- OptiGUI/src/main/kotlin/opekope2/optigui/internal/TextureReplacer.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/internal/TextureReplacer.kt
@@ -4,6 +4,10 @@ import com.google.common.collect.ImmutableMap
 import com.google.common.collect.ImmutableSet
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.world.ClientWorld
 import net.minecraft.util.Identifier
 import opekope2.optigui.internal.filter.TextureReplacerFilter
@@ -12,7 +16,8 @@ import opekope2.optigui.internal.util.OrderedListLruAccessor
 
 internal typealias ContainerId2FiltersMap = ImmutableMap<Identifier?, OrderedListLruAccessor<TextureReplacerFilter>>
 
-internal object TextureReplacer : ClientModInitializer, ClientTickEvents.EndWorldTick {
+internal object TextureReplacer : ClientModInitializer, ClientTickEvents.EndWorldTick, ScreenEvents.BeforeInit,
+    ScreenEvents.BeforeRender, ScreenEvents.AfterRender {
     private var filters: ContainerId2FiltersMap = ImmutableMap.of()
     private var replaceableTextures: ImmutableSet<Identifier> = ImmutableSet.of()
     private val replacementCache = mutableMapOf<Identifier, Identifier>()
@@ -48,10 +53,24 @@ internal object TextureReplacer : ClientModInitializer, ClientTickEvents.EndWorl
 
     override fun onInitializeClient() {
         ClientTickEvents.END_WORLD_TICK.register(this)
+        ScreenEvents.BEFORE_INIT.register(this)
     }
 
     override fun onEndTick(world: ClientWorld?) {
         if (!InteractionManager.isInteracting) return
         clearCache()
+    }
+
+    override fun beforeInit(client: MinecraftClient?, screen: Screen, scaledWidth: Int, scaledHeight: Int) {
+        ScreenEvents.beforeRender(screen).register(this)
+        ScreenEvents.afterRender(screen).register(this)
+    }
+
+    override fun beforeRender(screen: Screen?, drawContext: DrawContext?, mouseX: Int, mouseY: Int, tickDelta: Float) {
+        isReplacingTextures = true
+    }
+
+    override fun afterRender(screen: Screen?, drawContext: DrawContext?, mouseX: Int, mouseY: Int, tickDelta: Float) {
+        isReplacingTextures = false
     }
 }
