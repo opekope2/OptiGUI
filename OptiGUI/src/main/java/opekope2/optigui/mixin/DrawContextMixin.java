b4abf74a60d000bf58fee245d7a515fd69f60635
opekope2 <opekope2@gmail.com>
2024-12-10 00:32:28 +0100

    Mix into DrawContext instead of RenderSystem

---
+++ OptiGUI/src/main/java/opekope2/optigui/mixin/DrawContextMixin.java
@@ -0,0 +1,21 @@
+package opekope2.optigui.mixin;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.Identifier;
+import opekope2.optigui.internal.TextureReplacer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(value = DrawContext.class, priority = 800)
+abstract class DrawContextMixin {
+    @ModifyVariable(
+            method = "drawTexture(Lnet/minecraft/util/Identifier;IIIIIIIFFII)V",
+            at = @At("HEAD"),
+            index = 1,
+            argsOnly = true
+    )
+    private Identifier replaceTexture(Identifier id) {
+        return id != null ? TextureReplacer.replaceTexture(id) : null;
+    }
+}
