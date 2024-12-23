b6c0eeae9d8de9b08389481608b8f99e258e736e
opekope2 <opekope2@gmail.com>
2024-12-23 19:01:20 +0100

    Fix Generic3x3ContainerScreenHandlerMixin

--- OptiGUI/src/main/java/opekope2/optigui/mixin/Generic3x3ContainerScreenHandlerMixin.java
+++ OptiGUI/src/main/java/opekope2/optigui/mixin/Generic3x3ContainerScreenHandlerMixin.java
@@ -1,19 +1,21 @@
 package opekope2.optigui.mixin;
 
 import net.minecraft.inventory.Inventory;
-import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.Generic3x3ContainerScreenHandler;
 import net.minecraft.screen.ScreenHandler;
 import opekope2.optigui.screen.IRedstoneComparatorOutputGetterScreen;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(GenericContainerScreenHandler.class)
+@Mixin(Generic3x3ContainerScreenHandler.class)
 public abstract class Generic3x3ContainerScreenHandlerMixin implements IRedstoneComparatorOutputGetterScreen {
     @Shadow
-    public abstract Inventory getInventory();
+    @Final
+    private Inventory inventory;
 
     @Override
     public int optiGUI_getRedstoneComparatorOutput() {
-        return ScreenHandler.calculateComparatorOutput(getInventory());
+        return ScreenHandler.calculateComparatorOutput(inventory);
     }
 }
