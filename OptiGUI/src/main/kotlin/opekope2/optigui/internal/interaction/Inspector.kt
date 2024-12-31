0f7997e2664431ab2327ee54809e38c2bab7a0d5
opekope2 <opekope2@gmail.com>
2024-12-31 18:11:56 +0100

    Replace inspector ScreenMixin with Fabric API

--- OptiGUI/src/main/kotlin/opekope2/optigui/internal/interaction/Inspector.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/internal/interaction/Inspector.kt
@@ -7,70 +7,94 @@ import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.mojang.serialization.Encoder
 import com.mojang.serialization.JsonOps
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents
+import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gui.screen.Screen
 import net.minecraft.nbt.AbstractNbtList
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.nbt.NbtElement
 import opekope2.optigui.interaction.InteractionManager
 import opekope2.optigui.interaction.data.IInteractionData
+import opekope2.optigui.internal.INSPECTOR_KEY_BINDING
 import opekope2.optigui.resource.format.json.JsonFilterResource
 import opekope2.optigui.resource.load.ILoadTimeNbtSupplier
+import opekope2.optigui.screen.IRetexturableScreen
+import opekope2.optigui.toast.InspectorToast
 
-private val GSON = GsonBuilder().setLenient().setPrettyPrinting().create()
-private val NBT_FILTER_JSON_ENCODER: Encoder<NbtCompound> = NbtCompound.CODEC.comap(::transformNbtFilterKeys)
+object Inspector : ClientModInitializer, ScreenEvents.BeforeInit, ScreenKeyboardEvents.AfterKeyRelease {
+    private val GSON = GsonBuilder().setLenient().setPrettyPrinting().create()
+    private val NBT_FILTER_JSON_ENCODER: Encoder<NbtCompound> = NbtCompound.CODEC.comap(::transformNbtFilterKeys)
 
-private fun transformNbtFilterKeys(nbt: NbtElement): NbtElement = when (nbt) {
-    is NbtCompound -> transformNbtFilterKeys(nbt)
-    is AbstractNbtList<*> -> transformNbtFilterKeys(nbt)
-    else -> nbt
-}
-
-@Suppress("NOTHING_TO_INLINE") // Stack size
-private inline fun transformNbtFilterKeys(nbt: NbtCompound): NbtCompound {
-    val compound = NbtCompound()
-
-    for (key in nbt.keys) {
-        compound.put("@$key", transformNbtFilterKeys(nbt[key]!!))
+    private fun transformNbtFilterKeys(nbt: NbtElement): NbtElement = when (nbt) {
+        is NbtCompound -> transformNbtFilterKeys(nbt)
+        is AbstractNbtList<*> -> transformNbtFilterKeys(nbt)
+        else -> nbt
     }
 
-    return compound
-}
+    @Suppress("NOTHING_TO_INLINE") // Stack size
+    private inline fun transformNbtFilterKeys(nbt: NbtCompound): NbtCompound {
+        val compound = NbtCompound()
 
-@Suppress("NOTHING_TO_INLINE") // Stack size
-private inline fun transformNbtFilterKeys(nbt: AbstractNbtList<*>): NbtCompound = NbtCompound().apply {
-    for ((index, element) in nbt.withIndex()) {
-        put("@$index", transformNbtFilterKeys(element))
-    }
-}
+        for (key in nbt.keys) {
+            compound.put("@$key", transformNbtFilterKeys(nbt[key]!!))
+        }
 
-internal fun inspectInteraction(): String? {
-    return GSON.toJson(createJsonResource(InteractionManager.interactionData ?: return null))
-}
-
-internal fun createJsonResource(interactionData: IInteractionData): JsonElement? = JsonObject().apply {
-    addProperty(JsonFilterResource.CONTAINERS_KEY, interactionData.id.toString())
-    add(JsonFilterResource.TEXTURES_KEY, getLastRenderedTextures())
-    add(JsonFilterResource.LOAD_FILTER_KEY, getLoadTimeNbtFilter())
-    add(JsonFilterResource.FILTER_KEY, getInteractionNbtFilter() ?: return null)
-}
-
-private fun getLastRenderedTextures() = JsonObject().apply {
-    for (texture in InteractionManager.lastFrameRenderedTextures) {
-        addProperty(texture.toString(), "example:path/to/replacement.png")
-    }
-}
-
-private fun getLoadTimeNbtFilter(): JsonElement? {
-    val loadTimeNbt = NbtCompound()
-    for ((key, supplier) in ILoadTimeNbtSupplier) {
-        loadTimeNbt.put(key, supplier.get())
+        return compound
     }
 
-    return NBT_FILTER_JSON_ENCODER.encodeStart(JsonOps.INSTANCE, loadTimeNbt).result().orElse(null)
-}
+    @Suppress("NOTHING_TO_INLINE") // Stack size
+    private inline fun transformNbtFilterKeys(nbt: AbstractNbtList<*>): NbtCompound = NbtCompound().apply {
+        for ((index, element) in nbt.withIndex()) {
+            put("@$index", transformNbtFilterKeys(element))
+        }
+    }
 
-private fun getInteractionNbtFilter(): JsonElement? {
-    val texture = InteractionManager.lastFrameRenderedTextures.firstOrNull() ?: return null
-    val interaction = InteractionManager.createInteraction(texture) ?: return null
+    override fun onInitializeClient() {
+        ScreenEvents.BEFORE_INIT.register(this)
+    }
 
-    return NBT_FILTER_JSON_ENCODER.encodeStart(JsonOps.INSTANCE, interaction.createNbt()).result().orElse(null)
+    override fun beforeInit(client: MinecraftClient?, screen: Screen?, scaledWidth: Int, scaledHeight: Int) {
+        ScreenKeyboardEvents.afterKeyRelease(screen).register(this)
+    }
+
+    override fun afterKeyRelease(screen: Screen?, key: Int, scancode: Int, modifiers: Int) {
+        if (screen !is IRetexturableScreen) return
+        if (!INSPECTOR_KEY_BINDING.matchesKey(key, scancode)) return
+
+        val inspection = GSON.toJson(createJsonResource(InteractionManager.interactionData ?: return) ?: return)
+
+        MinecraftClient.getInstance().keyboard.clipboard = inspection
+        MinecraftClient.getInstance().toastManager.add(InspectorToast())
+    }
+
+    private fun createJsonResource(interactionData: IInteractionData): JsonElement? = JsonObject().apply {
+        addProperty(JsonFilterResource.CONTAINERS_KEY, interactionData.id.toString())
+        add(JsonFilterResource.TEXTURES_KEY, getLastRenderedTextures())
+        add(JsonFilterResource.LOAD_FILTER_KEY, getLoadTimeNbtFilter())
+        add(JsonFilterResource.FILTER_KEY, getInteractionNbtFilter() ?: return null)
+    }
+
+    private fun getLastRenderedTextures() = JsonObject().apply {
+        for (texture in InteractionManager.renderedTextures) {
+            addProperty(texture.toString(), "example:path/to/replacement.png")
+        }
+    }
+
+    private fun getLoadTimeNbtFilter(): JsonElement? {
+        val loadTimeNbt = NbtCompound()
+        for ((key, supplier) in ILoadTimeNbtSupplier) {
+            loadTimeNbt.put(key, supplier.get())
+        }
+
+        return NBT_FILTER_JSON_ENCODER.encodeStart(JsonOps.INSTANCE, loadTimeNbt).result().orElse(null)
+    }
+
+    private fun getInteractionNbtFilter(): JsonElement? {
+        val texture = InteractionManager.renderedTextures.firstOrNull() ?: return null
+        val interaction = InteractionManager.createInteraction(texture) ?: return null
+
+        return NBT_FILTER_JSON_ENCODER.encodeStart(JsonOps.INSTANCE, interaction.createNbt()).result().orElse(null)
+    }
 }
