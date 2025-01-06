f605618e51f7fdfe72ca6ea621d80c3750c0cb3f
opekope2 <opekope2@gmail.com>
2025-01-06 20:39:41 +0100

    Merge IBookScreenAccessor into BookScreenMixin
    Use mixin inheritance for LecternScreenMixin

--- OptiGUI/src/main/java/opekope2/optigui/mixin/screen/BookScreenMixin.java
+++ OptiGUI/src/main/java/opekope2/optigui/mixin/screen/BookScreenMixin.java
@@ -4,19 +4,22 @@ import net.minecraft.client.gui.screen.ingame.BookScreen;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.RegistryWrapper;
 import opekope2.optigui.interaction.InteractionManager;
-import opekope2.optigui.mixin.IBookScreenAccessor;
 import opekope2.optigui.screen.IRetexturableScreen;
 import opekope2.optigui.util.Constants;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BookScreen.class)
-public abstract class BookScreenMixin implements IBookScreenAccessor, IRetexturableScreen {
+public abstract class BookScreenMixin implements IRetexturableScreen {
+    @Accessor
+    protected abstract int getPageIndex();
+
     @Shadow
     protected abstract int getPageCount();
 
--- OptiGUI/src/main/java/opekope2/optigui/mixin/screen/LecternScreenMixin.java
+++ OptiGUI/src/main/java/opekope2/optigui/mixin/screen/LecternScreenMixin.java
@@ -1,25 +1,20 @@
 package opekope2.optigui.mixin.screen;
 
-import net.minecraft.client.gui.screen.ingame.BookScreen;
 import net.minecraft.client.gui.screen.ingame.LecternScreen;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.math.MathHelper;
-import opekope2.optigui.mixin.IBookScreenAccessor;
-import opekope2.optigui.screen.IRetexturableScreen;
 import opekope2.optigui.util.Constants;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(LecternScreen.class)
-public abstract class LecternScreenMixin extends BookScreen implements IRetexturableScreen, IBookScreenAccessor {
+public abstract class LecternScreenMixin extends BookScreenMixin {
     @Override
     public void optiGui_writeNbt(@NotNull NbtCompound compound, @NotNull RegistryWrapper.WrapperLookup lookup) {
-        IRetexturableScreen.super.optiGui_writeNbt(compound, lookup);
+        super.optiGui_writeNbt(compound, lookup);
 
-        float f = callGetPageCount() > 1 ? getPageIndex() / (callGetPageCount() - 1.0f) : 1.0f;
+        float f = getPageCount() > 1 ? getPageIndex() / (getPageCount() - 1.0f) : 1.0f;
         compound.putInt(Constants.COMPARATOR_OUTPUT_KEY, MathHelper.floor(f * 14.0f) + 1);
-        compound.putInt(Constants.CURRENT_PAGE_KEY, getPageIndex() + 1);
-        compound.putInt(Constants.PAGE_COUNT_KEY, callGetPageCount());
     }
 }
