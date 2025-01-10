838a0e397f017b439a7d4cfb41ab607c7063ca0c
opekope2 <opekope2@gmail.com>
2025-01-10 19:04:22 +0100

    Fix lectern inspector crash

--- OptiGUI/src/main/java/opekope2/optigui/mixin/screen/BookScreenMixin.java
+++ OptiGUI/src/main/java/opekope2/optigui/mixin/screen/BookScreenMixin.java
@@ -8,8 +8,8 @@ import opekope2.optigui.screen.ITextureChangeableScreen;
 import opekope2.optigui.util.Constants;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,8 +20,8 @@ public abstract class BookScreenMixin implements ITextureChangeableScreen {
     @Accessor
     protected abstract int getPageIndex();
 
-    @Shadow
-    protected abstract int getPageCount();
+    @Invoker
+    protected abstract int callGetPageCount();
 
     @Inject(method = "setPage", at = @At("RETURN"))
     private void clearInteractionCacheAfterPageChange(CallbackInfoReturnable<Boolean> cir) {
@@ -38,6 +38,6 @@ public abstract class BookScreenMixin implements ITextureChangeableScreen {
         ITextureChangeableScreen.super.optiGui_writeNbt(compound, lookup);
 
         compound.putInt(Constants.CURRENT_PAGE_KEY, getPageIndex() + 1);
-        compound.putInt(Constants.PAGE_COUNT_KEY, getPageCount());
+        compound.putInt(Constants.PAGE_COUNT_KEY, callGetPageCount());
     }
 }
--- OptiGUI/src/main/java/opekope2/optigui/mixin/screen/LecternScreenMixin.java
+++ OptiGUI/src/main/java/opekope2/optigui/mixin/screen/LecternScreenMixin.java
@@ -14,7 +14,7 @@ public abstract class LecternScreenMixin extends BookScreenMixin {
     public void optiGui_writeNbt(@NotNull NbtCompound compound, @NotNull RegistryWrapper.WrapperLookup lookup) {
         super.optiGui_writeNbt(compound, lookup);
 
-        float f = getPageCount() > 1 ? getPageIndex() / (getPageCount() - 1.0f) : 1.0f;
+        float f = callGetPageCount() > 1 ? getPageIndex() / (callGetPageCount() - 1.0f) : 1.0f;
         compound.putInt(Constants.COMPARATOR_OUTPUT_KEY, MathHelper.floor(f * 14.0f) + 1);
     }
 }
