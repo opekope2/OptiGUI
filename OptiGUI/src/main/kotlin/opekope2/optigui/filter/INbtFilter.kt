a6944dd787d61b33283278f03b11d482ab5baa04
opekope2 <opekope2@gmail.com>
2025-06-20 20:47:04 +0200

    Override and, negate, or in INbtFilter

--- OptiGUI/src/main/kotlin/opekope2/optigui/filter/INbtFilter.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/filter/INbtFilter.kt
@@ -6,4 +6,10 @@ import java.util.function.Predicate
 /**
  * Functional interface for filtering [NbtElement]s.
  */
-fun interface INbtFilter : Predicate<NbtElement>
+fun interface INbtFilter : Predicate<NbtElement> {
+    override fun and(other: Predicate<in NbtElement>) = INbtFilter { test(it) && other.test(it) }
+
+    override fun negate() = INbtFilter { !test(it) }
+
+    override fun or(other: Predicate<in NbtElement>) = INbtFilter { test(it) || other.test(it) }
+}
