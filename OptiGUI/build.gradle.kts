0776b7b10e8096d06081a3ff10d3e9d2d122fc82
opekope2 <opekope2@gmail.com>
2025-07-01 21:45:35 +0200

    Generate translations enum from JSON

--- OptiGUI/build.gradle.kts
+++ OptiGUI/build.gradle.kts
@@ -1,3 +1,4 @@
+import groovy.json.JsonSlurper
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -106,6 +107,46 @@ tasks {
         useJUnitPlatform()
         testLogging {
             events("PASSED", "SKIPPED", "FAILED")
+        }
+    }
+
+    val generateI18n by registering {
+        val jsonPath = "src/main/resources/assets/optigui/lang/en_us.json"
+        val outFile = project.layout.buildDirectory.file("generated/opekope2/optigui/internal/I18n.kt")
+
+        inputs.file(jsonPath)
+        outputs.file(outFile)
+
+        doLast {
+            val json = JsonSlurper().parse(file(jsonPath)) as Map<String, String>
+            val members = json.entries.joinToString(separator = ",\n    ") { (key, value) ->
+                val k = key.uppercase().replace("""[^a-zA-Z0-9]""".toRegex(), "_")
+                """$k("$key", "$value")"""
+            }
+            val enum = """
+                package opekope2.optigui.internal
+                
+                import net.minecraft.text.Text
+                
+                enum class I18n(private val key: String, private val fallback: String) {
+                    %s;
+
+                    fun getTranslation(vararg args: Any?): String = Text.translatableWithFallback(key, fallback, *args).getString()
+                }
+            """.trimIndent().format(members)
+
+            file(outFile).writeText(enum)
+        }
+    }
+
+    val compileKotlin by getting { dependsOn(generateI18n) }
+    val sourcesJar by getting { dependsOn(generateI18n) }
+}
+
+sourceSets {
+    main {
+        kotlin {
+            srcDir(project.layout.buildDirectory.dir("generated"))
         }
     }
 }
--- OptiGUI/src/main/kotlin/opekope2/optigui/util/I18n.kt
+++
@@ -1,9 +0,0 @@
-package opekope2.optigui.util
-
-import net.minecraft.text.Text
-
-/**
- * @see Text.translatableWithFallback
- */
-fun i18n(key: String, fallback: String, vararg args: Any?): String =
-    Text.translatableWithFallback(key, fallback, *args).string
