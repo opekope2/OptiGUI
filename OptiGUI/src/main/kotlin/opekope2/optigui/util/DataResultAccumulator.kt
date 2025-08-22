1cc142eaa3f6e288cd666564b4804d005f7b66c5
opekope2 <20487556+opekope2@users.noreply.github.com>
2025-08-23 00:18:36 +0200

    Add DataResultAccumulator

---
+++ OptiGUI/src/main/kotlin/opekope2/optigui/util/DataResultAccumulator.kt
@@ -0,0 +1,45 @@
+package opekope2.optigui.util
+
+import com.google.common.collect.ImmutableList
+import com.mojang.serialization.DataResult
+import opekope2.optigui.util.DataResultAccumulator.Companion.createCollector
+import java.util.function.Function
+import java.util.stream.Collector
+import java.util.stream.Stream
+
+/**
+ * An intermediate accumulation type for [Collector], which accumulates [DataResult]s to a single [DataResult].
+ *
+ * @see createCollector
+ */
+class DataResultAccumulator<T> {
+    private var builder = DataResult.success(ImmutableList.builder<T>())
+
+    private fun add(value: DataResult<T>) {
+        builder = builder.apply2(ImmutableList.Builder<T>::add, value)
+    }
+
+    private fun combine(a: DataResultAccumulator<T>): DataResultAccumulator<T> {
+        builder = builder.apply2({ a, b -> a.addAll(b.build()) }, a.builder)
+        return this
+    }
+
+    private fun <TResult> finish(finisher: Function<List<T>, TResult>): DataResult<TResult> =
+        builder.map { finisher.apply(it.build()) }
+
+    companion object {
+        /**
+         * Creates a [Collector] for a [Stream] of [DataResult]s.
+         *
+         * @param finisher A function combining the [DataResult.result]s into a single object
+         */
+        @JvmStatic
+        fun <TStream, TResult> createCollector(finisher: Function<List<TStream>, TResult>): Collector<DataResult<TStream>, DataResultAccumulator<TStream>, DataResult<TResult>> =
+            Collector.of(
+                ::DataResultAccumulator,
+                DataResultAccumulator<TStream>::add,
+                DataResultAccumulator<TStream>::combine,
+                { it.finish(finisher) }
+            )
+    }
+}
