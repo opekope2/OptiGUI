14a240943581b84d0796c9db8dbaea8168ce824c
opekope2 <20487556+opekope2@users.noreply.github.com>
2026-01-16 20:47:29 +0100

    Update DynamicNbtComparerFilter docs

--- OptiGUI/src/main/kotlin/opekope2/optigui/filter/DynamicNbtComparerFilter.kt
+++ OptiGUI/src/main/kotlin/opekope2/optigui/filter/DynamicNbtComparerFilter.kt
@@ -8,9 +8,9 @@ import opekope2.optigui.filter.transformer.NbtTransformerChain
 import java.util.*
 
 /**
- * An NBT filter, which compares an NBT element with another NBT element extracted from the root NBT element.
+ * An NBT filter, which compares an NBT element with another NBT element.
  *
- * @param transformerChain The NBT transformer chain used to extract the other NBT element from the root NBT element
+ * @param transformerChain The NBT transformer chain used to extract the other NBT element
  * @param type The type describing this filter
  * @see INbtComparer
  * @see ConstantNbtComparerFilter
