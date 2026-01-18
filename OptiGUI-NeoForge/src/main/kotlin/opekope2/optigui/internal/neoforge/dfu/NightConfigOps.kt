544d0c92314f8f836523c3b7608ed66e41a34182
opekope2 <20487556+opekope2@users.noreply.github.com>
2026-01-18 23:10:15 +0100

    Update Temporal conversion

--- OptiGUI-NeoForge/src/main/kotlin/opekope2/optigui/internal/neoforge/dfu/NightConfigOps.kt
+++ OptiGUI-NeoForge/src/main/kotlin/opekope2/optigui/internal/neoforge/dfu/NightConfigOps.kt
@@ -7,8 +7,12 @@ import com.mojang.serialization.JavaOps
 import it.unimi.dsi.fastutil.bytes.ByteList
 import it.unimi.dsi.fastutil.ints.IntList
 import it.unimi.dsi.fastutil.longs.LongList
+import it.unimi.dsi.fastutil.objects.Object2LongArrayMap
+import it.unimi.dsi.fastutil.objects.Object2LongMap
 import net.minecraft.resources.DelegatingOps
+import java.time.temporal.ChronoField
 import java.time.temporal.Temporal
+import java.time.temporal.TemporalAccessor
 
 internal object NightConfigOps : DelegatingOps<Any>(JavaOps.INSTANCE) {
     override fun <U : Any> convertTo(outOps: DynamicOps<U>, input: Any): U = when (input) {
@@ -17,11 +21,17 @@ internal object NightConfigOps : DelegatingOps<Any>(JavaOps.INSTANCE) {
         is List<*> -> convertList(outOps, input)
 
         is Config -> convertMap(outOps, input.valueMap())
-        is Temporal -> outOps.createString(input.toString())
+        is Temporal -> convertMap(outOps, input.getChronoFields())
         is NullObject -> outOps.empty()
         is Collection<*> -> convertList(outOps, input.toList())
         is Enum<*> -> outOps.createString(input.name)
 
         else -> super.convertTo(outOps, input)
     }
+
+    private fun TemporalAccessor.getChronoFields(): Object2LongMap<String> =
+        ChronoField.entries.fold(Object2LongArrayMap(ChronoField.entries.size)) { acc, field ->
+            acc[field.toString()] = getLong(field)
+            acc
+        }
 }
